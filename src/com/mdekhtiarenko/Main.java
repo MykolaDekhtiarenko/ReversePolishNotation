cbae174d793d6eb8ca6c24635136465094b5b855
Mykola Dekhtiarenko <nnick1212@gmail.com>
2017-11-08 11:16:37 +0200

    almost done, includes bug with functions

--- src/com/mdekhtiarenko/Main.java
+++ src/com/mdekhtiarenko/Main.java
@@ -1,51 +1,17 @@
 package com.mdekhtiarenko;
 
-import java.util.List;
-import java.util.Stack;
 
 public class Main {
 
     public static void main(String [] args){
-        List<String> rpn = new ReversePolishNotation().parse("5+6*(8-4)");
-
-        System.out.println(evalRPN(rpn));
-    }
-
-    public static int evalRPN(List<String> tokens) {
-
-        int returnValue = 0;
-
-        String operators = "+-*/";
-
-        Stack<String> stack = new Stack<String>();
-
-        for(String t : tokens){
-            if(!operators.contains(t)){
-                stack.push(t);
-            }else{
-                int a = Integer.valueOf(stack.pop());
-                int b = Integer.valueOf(stack.pop());
-                int index = operators.indexOf(t);
-                switch(index){
-                    case 0:
-                        stack.push(String.valueOf(a+b));
-                        break;
-                    case 1:
-                        stack.push(String.valueOf(b-a));
-                        break;
-                    case 2:
-                        stack.push(String.valueOf(a*b));
-                        break;
-                    case 3:
-                        stack.push(String.valueOf(b/a));
-                        break;
-                }
-            }
+        ReversePolishNotation rpn = null;
+        try {
+            rpn = new ReversePolishNotation("-5+6*(8-4)+sin(1)");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
-
-        returnValue = Integer.valueOf(stack.pop());
-
-        return returnValue;
-
+        System.out.println(rpn.calculate());
     }
+
+
 }
--- src/com/mdekhtiarenko/ReversePolishNotation.java
+++ src/com/mdekhtiarenko/ReversePolishNotation.java
@@ -7,28 +7,30 @@ import java.util.*;
  */
 public class ReversePolishNotation {
 
-    private static final String NEGATIVE_SIGN = "nv";
+    private static final String NEGATIVE_SIGN = "ns";
     private String operators = "+-*/^";
     private String brackets = "()";
-    private List<String> functions;
+    private List<String> functions = Arrays.asList("sin", "cos", "sqrt");
 
-    private List<String> reverseNotation = new ArrayList<>();
+    private List<String> reversePolishNotation = new ArrayList<>();
     private Deque<String> operationStack = new ArrayDeque<>();
 
-    public ReversePolishNotation() {
-        functions = new ArrayList<>();
-        functions.add("sin");
-        functions.add("cos");
-        functions.add("sqrt");
+    public ReversePolishNotation(String expr) throws Exception {
+        parse(expr);
     }
 
-//    private boolean isDelimiter(String token) {
-//        if (token.length() != 1) return false;
-//        for (int i = 0; i < (operators+brackets).length(); i++) {
-//            if (token.charAt(0) == (operators+brackets).charAt(i)) return true;
-//        }
-//        return false;
-//    }
+    public List<String> getReversePolishNotation() {
+        return reversePolishNotation;
+    }
+
+
+    private boolean isDelimiter(String token) {
+        if (token.length() != 1) return false;
+        for (int i = 0; i < (operators+brackets).length(); i++) {
+            if (token.charAt(0) == (operators+brackets).charAt(i)) return true;
+        }
+        return false;
+    }
 
     private boolean isOperator(String token) {
         if(token.equals("")) return false;
@@ -39,7 +41,7 @@ public class ReversePolishNotation {
     }
 
     private boolean isOperand(String token){
-        return token != null && token.matches("[n]?\\d*\\.?\\d+");
+        return token != null && token.matches("[+-]?\\d*\\.?\\d+");
     }
 
     private boolean isFunction(String token) {
@@ -47,30 +49,30 @@ public class ReversePolishNotation {
         return false;
     }
 
-    public int precedence(String token){
+    private int precedence(String token){
         if (token.equals("(")) return 1;
         if (token.equals("+") || token.equals("-")) return 2;
         if (token.equals("*") || token.equals("/")) return 3;
         return 4;
     }
 
-    public List<String> parse(String expr) {
-
+    private void parse(String expr) throws Exception {
         StringTokenizer tokenizer = new StringTokenizer(expr, (operators+brackets), true);
+        String previous = "";
+        String current = "";
+
         while (tokenizer.hasMoreTokens()){
-            String previous = "";
-            String current = tokenizer.nextToken();
+            current = tokenizer.nextToken();
             if (!tokenizer.hasMoreTokens() && isOperator(current))
-                throw new RuntimeException("Wrong expression!");
+                throw new Exception("Wrong expression!");
 
             if(current.equals(" ")) continue;
 
             if(isOperand(current)){
-                System.out.println(previous+" "+current);
                 if(previous.equals(NEGATIVE_SIGN))
-                    reverseNotation.add("-"+current);
+                    reversePolishNotation.add("-"+current);
                 else
-                    reverseNotation.add(current);
+                    reversePolishNotation.add(current);
             }
             else if(isFunction(current)){
                 operationStack.add(current);
@@ -81,12 +83,12 @@ public class ReversePolishNotation {
             else if(current.equals(")")){
                 String currentOperator = operationStack.pollLast();
                 while (!operationStack.isEmpty()&&!currentOperator.equals("(")) {
-                    reverseNotation.add(currentOperator);
+                    reversePolishNotation.add(currentOperator);
                     currentOperator = operationStack.pollLast();
                 }
             }
             else{
-                if(isOperator(previous)&&current.equals("-")) {
+                if((previous.equals("")||isDelimiter(previous))&&current.equals("-")) {
                     previous = NEGATIVE_SIGN;
                     continue;
                 }
@@ -94,10 +96,8 @@ public class ReversePolishNotation {
             }
             previous = current;
         }
-        reverseNotation.addAll(operationStack);
-        return reverseNotation;
+        reversePolishNotation.addAll(operationStack);
     }
-
 
     private void addOperatorToStack(String operator){
         if(operationStack.isEmpty()){
@@ -105,12 +105,73 @@ public class ReversePolishNotation {
         }
         else{
             if(precedence(operationStack.getLast())>precedence(operator)){
-                reverseNotation.add(operationStack.pollLast());
+                reversePolishNotation.add(operationStack.pollLast());
                 addOperatorToStack(operator);
             }
             else{
                 operationStack.add(operator);
             }
         }
+    }
+
+    public double calculate() {
+        double returnValue = 0;
+        Stack<String> stack = new Stack<>();
+        for(String t : reversePolishNotation){
+            System.out.println(t);
+            if(isOperand(t)){
+                stack.push(t);
+            }
+            else{
+                if(isFunction(t)){
+                    int index = functions.indexOf(t);
+                    double a = Double.valueOf(stack.pop());
+                    System.out.println("F:"+a);
+                    switch (index){
+                        case 0:
+                            stack.push(String.valueOf(Math.sin(Math.toRadians(a))));
+                            break;
+                        case 1:
+                            stack.push(String.valueOf(Math.cos(Math.toRadians(a))));
+                            break;
+                        case 2:
+                            stack.push(String.valueOf(Math.sqrt(a)));
+                            break;
+                    }
+                }
+                else{
+                    int index = operators.indexOf(t);
+                    double a = Double.valueOf(stack.pop());
+                    double b = Double.valueOf(stack.pop());
+                    switch(index){
+                        case 0:
+                            stack.push(String.valueOf(a+b));
+                            break;
+                        case 1:
+                            stack.push(String.valueOf(b-a));
+                            break;
+                        case 2:
+                            stack.push(String.valueOf(a*b));
+                            break;
+                        case 3:
+                            stack.push(String.valueOf(b/a));
+                            break;
+                        case 4:
+                            stack.push(String.valueOf((int)b^(int)a));
+                            break;
+                    }
+                }
+
+            }
+        }
+        returnValue = Integer.valueOf(stack.pop());
+
+        return returnValue;
+
+    }
+
+    @Override
+    public String toString() {
+        return reversePolishNotation.toString();
     }
 }
